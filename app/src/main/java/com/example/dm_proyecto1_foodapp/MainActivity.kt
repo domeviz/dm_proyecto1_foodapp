ff856bdddca3abf6270c13c66851c9d7fb0a1520
Domenica Vizcarra <domeviz3410@gmail.com>
2023-05-22 12:52:16 -0500

    Proyecto 1 - Food App

---
+++ app/src/main/java/com/example/dm_proyecto1_foodapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dm_proyecto1_foodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
